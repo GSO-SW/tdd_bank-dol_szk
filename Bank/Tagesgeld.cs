b8b011fbb4b7d9b4174d228bf8916f19d5bc30f6
Daniel Dolynskyy <danielser.d2@gso.schule.koeln>
2022-12-01 08:37:24 +0100

    D3 - Neue Testklasse: TagesgeldTests & Neue Klasse: Tagesgeld hinzugefügt

---
+++ Bank/Tagesgeld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Tagesgeld
+    {
+        private Konto konto;
+
+        public Tagesgeld(Konto konto)
+        {
+            this.konto = konto;
+        }
+    }
+}
